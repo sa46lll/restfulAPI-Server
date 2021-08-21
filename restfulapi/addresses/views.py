050c090040204d12e3195237c9275017d7b6d536
sa46lll <9812smh@naver.com>
2021-08-21 15:46:49 +0900

    [Feat] 단건 기능 추가(조회, 수정, 삭제)

--- restfulapi/addresses/views.py
+++ restfulapi/addresses/views.py
@@ -5,8 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from rest_framework.parsers import JSONParser
 
-from addresses.models import Addresses
-from addresses.serializers import AddressesSerializer
+from .models import Addresses
+from .serializers import AddressesSerializer
 
 
 @csrf_exempt
@@ -23,3 +23,26 @@ def address_list(request):
             serializer.save() # 객체 생성
             return JsonResponse(serializer.data, status=201) # 성공
         return JsonResponse(serializer.errors, status=400) # 실패
+
+
+@csrf_exempt
+def address(request, pk):
+    obj = Addresses.objects.get(pk=pk) # filter은 여러개
+
+    if request.method == 'GET': # 데이터 조회
+        serializer = AddressesSerializer(obj)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'PUT': # 데이터 생성
+        data = JSONParser().parse(request)
+        serializer = AddressesSerializer(obj, data=data)
+        if serializer.is_valid():
+            serializer.save() # 객체 생성
+            return JsonResponse(serializer.data, status=201) # 성공
+        return JsonResponse(serializer.errors, status=400) # 실패
+
+    elif request.method =='DELETE':
+        obj.delete()
+        return HttpResponse(status=204)
+
+
--- restfulapi/restfulapi/urls.py
+++ restfulapi/restfulapi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+# from ..addresses import views
 from addresses import views
 
 urlpatterns = [
-    path(r'addresses/', views.address_list),
-    path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('addresses/', views.address_list), # r -> 인코딩 형식
+    path('addresses/<int:pk>/', views.address),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
